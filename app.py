3aba9c0acbe3af267e8d9deff939ab5c44bfa1d7
ArumilliLalitha <arumillilalitha2005@gmail.com>
2026-04-05 14:45:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,9 +58,27 @@ for col in df.columns:
 
 X = df.drop("class", axis=1)
 y = df["class"]
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+st.write("Accuracy:", accuracy)
+
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X, y)
-
 # Collect user input
 user_input = {}
 st.sidebar.header("Choose mushroom features:")
